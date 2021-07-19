485ea49fa96f04d614cac7a00a721f8e94c966e6
Rafael Levi <levi.costa@sof.to>
2021-07-19 16:57:26 -0300

    adjusts

--- src/people/dto/list-user.dto.ts
+++ src/people/dto/list-user.dto.ts
@@ -3,5 +3,5 @@ export class ListPersonDto {
   id: number;
   nome: string;
   cpf: string;
-  address: Address[];
+  address: Address[]; //to do: vamos supor que há uma tabela de cidades relacionada com endereço.   olhar Lush-Backend/src/reservation/reservation-periods/dtos/response-reservation-period.dto.ts como exemplo
 }
