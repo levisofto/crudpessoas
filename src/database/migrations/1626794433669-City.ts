a4883436148375750f4244e646db319c596be859
rafaellevissa <rafaellevissa@gmail.com>
2021-07-20 12:59:52 -0300

    migration city created

---
+++ src/database/migrations/1626794433669-City.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class City1626794433669 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'city',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          isGenerated: true,
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('city');
+  }
+
+}
